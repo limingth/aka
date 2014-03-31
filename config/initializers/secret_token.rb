5cf5a73d35ad2b03d84976bbec8043b81562003a
Liu Jiyang <2411mail@gmail.com>
2014-03-31 15:31:04 +0800

    init the pp2code project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aka::Application.config.secret_token = '1cb803b125b1f425cbccba57ef133f2f1bff1b2100e6e9b5e7fa627c2db77255903a412bed853b6ba89eded926a0ef1a171f02620b61f3c0f1096e03b1247171'
